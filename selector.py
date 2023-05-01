a046e8039e23e5f33271a76e040ea4141022c5f2
junga15 <jungakim1551@gmail.com>
2023-05-01 14:22:46 +0900

    feat: random sampling select 6 of total 45

--- selector.py
+++ selector.py
@@ -6,7 +6,7 @@ ran_num = random.randint(0,9)
 
 for i in range(6):
     while ran_num in list:
-        ran_num = random.randint(0,9)
+        ran_num = random.randint(0,46)
     list.append(ran_num)
 
 print(list)
